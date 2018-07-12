99a725bc1b92553c273a87788566180117ded524
song <35187276+scarlett0507@users.noreply.github.com>
2018-07-12 16:00:46 +0900

    0712

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//获取系统时间
+var newDate = '';
+getLangDate();
+//值小于10时，在前面补0
+function dateFilter(date){
+    if(date < 10){return "0"+date;}
+    return date;
+}
+function getLangDate() {
+    var dateObj = new Date(); //表示当前系统时间的Date对象
+    var year = dateObj.getFullYear(); //当前系统时间的完整年份值
+    var month = dateObj.getMonth() + 1; //当前系统时间的月份值
+    var date = dateObj.getDate(); //当前系统时间的月份中的日
+    var day = dateObj.getDay(); //当前系统时间中的星期值
+    var weeks = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"];
+    var week = weeks[day]; //根据星期值，从数组中获取对应的星期字符串
+    var hour = dateObj.getHours(); //当前系统时间的小时值
+    var minute = dateObj.getMinutes(); //当前系统时间的分钟值
+    var second = dateObj.getSeconds(); //当前系统时间的秒钟值
+    newDate = dateFilter(year) + "年" + dateFilter(month) + "月" + dateFilter(date) + "日 " + " " + dateFilter(hour) + ":" + dateFilter(minute) + ":" + dateFilter(second);
+    document.getElementById("nowTime").innerHTML = "こんにちは、今は： " + newDate + "　" + week;
+    setTimeout("getLangDate()", 1000);
+}
